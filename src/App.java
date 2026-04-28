2c656a48e359b7d535f6e6e0132e788c9a41c24d
Visitante UniBH <visitantee1@ulife.com.br>
2026-04-28 20:51:33 -0300

    aula09: funcoes e arrays

---
+++ src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static int encontrarMaior(int[] array) {
+        int maior = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maior) {
+                maior = array[i];
+            }
+        }
+
+        return maior;
+    }
+
+    public static void main(String[]args) {
+        int[] valores = {10,20,30,};
+        int resultado =  encontrarMaior(valores);
+        System.out.println("valor maior:" + resultado);
+
+    }
+}
